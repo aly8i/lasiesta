35113b3905b88de7e6b4cc32c0ce5c198d20363a
aly8i <ABY00003@students.aust.edu.lb>
2023-05-24 13:54:11 +0300

    v63

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -68,6 +68,7 @@ const Navbar = () => {
         dispatch(addSocial({img:res.data.img,username:res.data.username,fullname:res.data.username}));
     }).catch(async (err)=>{
     if(session){
+        console.log("session available");
         await postUser(session.user).then(async (data)=>{
           console.log("5");
           dispatch(addSocial({img:session.user.image,username:session.user.name,fullname:session.user.name}));
