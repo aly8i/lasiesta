c3850e0359b98c3721e9257abe2f762e5fb6ffa6
aly8i <ABY00003@students.aust.edu.lb>
2023-05-24 15:43:12 +0300

    v74

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,7 +1,7 @@
 import { deleteCookie } from "cookies-next";
 const handler = async(req, res) => {
 
-  deleteCookie('accessToken',{req,res,path:'/',secure:true,sameSite:"strict"});
+  deleteCookie(req,res,'accessToken',{path:'/',secure:true,sameSite:"strict"});
     res.status(200).json("logged out");
 }
 export default handler;
