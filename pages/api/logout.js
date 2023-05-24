0ea6aa3dbffe54f189003dc3a9f86180578c87c6
aly8i <ABY00003@students.aust.edu.lb>
2023-05-24 16:24:32 +0300

    v76

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,7 +1,7 @@
 import { deleteCookie } from "cookies-next";
 const handler = async(req, res) => {
 
-  deleteCookie('accessToken', "", { req, res, path: '/', httpOnly: true, sameSite: "strict" });
+  deleteCookie('accessToken', "", { req, res, path: '/', httpOnly: true,domain:`${process.env.NEXT_PUBLIC_BASE_URL}`, sameSite: "strict" });
     res.status(200).json("logged out");
 }
 export default handler;
--- pages/api/users/find/index.js
+++ pages/api/users/find/index.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         try {
           const user = await User.findOne({'googleID': decoded.googleID});
           const access = await generateAccessToken(user);
-          await setCookie('accessToken',access,{req,res,maxAge: process.env.NEXT_PUBLIC_COOKIE_AGE,path:'/',httpOnly:true,sameSite:"strict"});
+          await setCookie('accessToken',access,{req,res,maxAge: process.env.NEXT_PUBLIC_COOKIE_AGE,path:'/',domain:`${process.env.NEXT_PUBLIC_BASE_URL}`,httpOnly:true,sameSite:"strict"});
           res.status(200).json(user);
         } catch (err) {
           res.status(500).json(err);
