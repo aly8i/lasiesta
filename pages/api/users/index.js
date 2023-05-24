0ae5369e0893e6ed2ba74b158172448c73701497
aly8i <ABY00003@students.aust.edu.lb>
2023-05-24 14:06:01 +0300

    v64

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -21,30 +21,19 @@ export default AuthorizedGet( async function handler(req, res) {
   if (method === "POST") {
     verify(req.body.jwt,process.env.NEXT_PUBLIC_JWT_SECRET,async function(err,decoded){
       if(!err && decoded) {
-    try {
-      const user = await User.create({      
-        googleID:decoded.googleID,
-        username:decoded.username,
-        fullname:decoded.fullname,
-        img:decoded.img
-      });
-      const access = generateAccessToken(user);
-      try {
-        await Token.create({value:access,userID:user._id});
-        res.setHeader("Set-Cookie",cookie.serialize("accessToken", access, {
-          maxAge: 2*24*60*60*1000,
-          sameSite: "strict",
-          httpOnly: true,
-          path: "/",
-        })
-      );
-      } catch (err) {
-        res.status(900).json(err);
+        var user = {}
+        try {
+           user =  await User.create({      
+            googleID:decoded.googleID,
+            username:decoded.username,
+            fullname:decoded.fullname,
+            img:decoded.img
+          });
+          } catch (err) {
+            res.status(900).json(err);
+          }
+        res.status(201).json(user);
       }
-      res.status(201).json(user);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }})
+    })
   }
 });
