ccb775fa0e40193eff596ca47f81b34fc69d8eba
aly8i <ABY00003@students.aust.edu.lb>
2023-05-24 14:15:14 +0300

    v68

--- functions/generateAccessToken.js
+++ functions/generateAccessToken.js
@@ -2,6 +2,7 @@ import {sign} from 'jsonwebtoken';
 const generateAccessToken = async(user) => {
     const payload = {sub:user._id , img:user.img , username:user.username , role:user.role};
     const jwt = sign(payload,process.env.NEXT_PUBLIC_JWT_SECRET,{expiresIn: '1d'})
+    console.log({jwt,payload})
     return jwt;
 };
 export default generateAccessToken;
