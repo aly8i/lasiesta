04029ed8e66a3c458f9419db0afa2b6c36763085
aly8i <ABY00003@students.aust.edu.lb>
2023-05-24 16:10:58 +0300

    v76

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,7 +1,7 @@
 import { setCookie } from "cookies-next";
 const handler = async(req, res) => {
 
-  setCookie('accessToken',"",{req,res,path:'/',maxAge:0,secure:true,sameSite:"strict"});
+  setCookie('accessToken',"",{req,res,path:'/',maxAge:0,httpOnly:true,sameSite:"strict"});
     res.status(200).json("logged out");
 }
 export default handler;
